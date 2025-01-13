c912d3a6aac0a2544936e21a6cf9eff8fd3b09c8
oriporatt <oriporatt@gmail.com>
2025-01-13 12:01:16 +0200

    Update width layout for header

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -20,13 +20,19 @@ export function AppHeader() {
 
 	return (
 		<header className="app-header full">
+			
+			<NavLink to="/" className="side-menu">
+				<img src="/img/menu.svg" alt="menu" className="menu-img"/>
+			</NavLink>
+
 			<NavLink to="/" className="logo">
 				<img src="/img/fiverr_logo.svg" alt="Fiverr Logo" className="logo-img"/>
 			</NavLink>
+			<div className='search-box'></div>
 			<nav>
 
-				<NavLink to="gig">Explore Gigs</NavLink>
-				<NavLink to="">Become a Seller</NavLink>
+				<NavLink className='gig-link' to="gig">Explore Gigs</NavLink>
+				<NavLink className='become-seller' to="">Become a Seller</NavLink>
 
 				{/* {user?.isAdmin && <NavLink to="/admin">Admin</NavLink>} */}
 
@@ -41,7 +47,7 @@ export function AppHeader() {
 						<button onClick={onLogout}>logout</button>
 					</div>
 				)}
-				<NavLink to=""><button>Join</button></NavLink> 
+				<NavLink className='join' to=""><button>Join</button></NavLink> 
 			</nav>
 		
 		</header>
