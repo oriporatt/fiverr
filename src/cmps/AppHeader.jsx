f934a143d59b154b552bf3266b484e52eaf2f5b3
oriporatt <oriporatt@gmail.com>
2025-01-14 15:07:01 +0200

    Made store property to define search box and show searchbox example on header only in HOME

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -1,13 +1,18 @@
-import { Link, NavLink } from 'react-router-dom'
+import { Link, NavLink ,useLocation} from 'react-router-dom'
 import { useNavigate } from 'react-router'
 import { useSelector } from 'react-redux'
 import { showErrorMsg, showSuccessMsg } from '../services/event-bus.service'
 import { logout } from '../store/actions/user.actions'
 
 export function AppHeader() {
+	const location = useLocation();
+	const isHomePage = location.pathname==='/'
+	console.log(isHomePage)
 	const user = useSelector(storeState => storeState.userModule.user)
+	const searchBoxPos = useSelector(storeState => storeState.systemModule.searchBoxPosition)
+	
 	const navigate = useNavigate()
-
+	let showSearchOnTop=false
 	async function onLogout() {
 		try {
 			await logout()
@@ -17,7 +22,9 @@ export function AppHeader() {
 			showErrorMsg('Cannot logout')
 		}
 	}
-
+	if (searchBoxPos==='top' && isHomePage){
+		showSearchOnTop=true
+	}
 	return (
 		<header className="app-header main-container">
 			<div className='header-elements'>
@@ -28,6 +35,11 @@ export function AppHeader() {
 				<NavLink to="/" className="logo">
 					<img src="/img/fiverr_logo.svg" alt="Fiverr Logo" className="logo-img"/>
 				</NavLink>
+
+				{showSearchOnTop&&<div className='topSearchBox'>
+					<p>Top Search Box</p>
+				</div>}
+
 				<nav>
 
 					<NavLink className='gig-link' to="gig">Explore Gigs</NavLink>
--- src/store/reducers/system.reducer.js
+++ src/store/reducers/system.reducer.js
@@ -1,8 +1,11 @@
 export const LOADING_START = 'LOADING_START'
 export const LOADING_DONE = 'LOADING_DONE'
+export const SET_SEARCH_BOX_POS = 'SET_SEARCH_BOX_POS'
+
 
 const initialState = {
-  isLoading: false
+  isLoading: false,
+  searchBoxPosition: 'bottom'
 }
 
 export function systemReducer (state = initialState, action = {}) {
@@ -11,6 +14,9 @@ export function systemReducer (state = initialState, action = {}) {
       return { ...state, isLoading: true }
     case LOADING_DONE:
       return { ...state, isLoading: false }
+    case SET_SEARCH_BOX_POS:
+      return { ...state, searchBoxPosition: action.searchBoxPosition }
     default: return state
   }
 }
+
