f0aa322e2575d3460b5242245d0a1aa67fc8c189
oriporatt <oriporatt@gmail.com>
2025-01-21 12:59:40 +0200

    fixed z ndex

--- src/cmps/GigPreview.jsx
+++ src/cmps/GigPreview.jsx
@@ -2,7 +2,11 @@ import { Link } from 'react-router-dom'
 
 export function GigPreview({ gig }) {
     return <article className="gig-preview">
-        <img src={gig.imgs[0]} alt="Description of image" />
+        <div className="img-container">
+            <img src={gig.imgs[0]} alt="Description of image" />
+
+        </div>
+
         <header>
             <Link to={`/gig/${gig._id}`}>{gig.owner.fullname}</Link>
         </header>
