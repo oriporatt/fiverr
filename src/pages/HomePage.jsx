11f993029449ba2b0fd2e1d052c6e2506e75940c
oriporatt <oriporatt@gmail.com>
2025-01-24 18:07:02 +0200

    Fixed home layout

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -74,7 +74,7 @@ export function HomePage() {
         })
     }
     return (
-        <section className='home-page  '>
+        <section className='home-page '>
             
 
             <div className="main-home-search ">
@@ -82,6 +82,7 @@ export function HomePage() {
                     <h1>Scale your professional workforce with <span>freelancers</span></h1>
                     {/* <h1>workforce with <span>freelancers</span></h1> */}
                 </div>
+
                 <div className="search-container " ref={searchBoxEl} >
                     <input type="text" placeholder="Search for any service..."/>
                     <img src="/img/search-icon.svg" alt="Search" className='search-icon' onClick={handleSearchInput} />
