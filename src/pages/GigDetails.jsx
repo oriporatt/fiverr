2b5a37260b995616f9a0ccda6301bf7bf2c484fc
oriporatt <oriporatt@gmail.com>
2025-03-06 21:40:29 +0200

    done nav bar and owner details

--- src/pages/GigDetails.jsx
+++ src/pages/GigDetails.jsx
@@ -1,10 +1,15 @@
+import React from 'react';
 import { useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams,NavLink } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { gigService } from '../services/gig'
 import { showSuccessMsg, showErrorMsg } from '../services/event-bus.service'
 import { loadGig } from '../store/actions/gig.actions'
+import Home from '../assets/svgs/home.svg?react'
+import BlackStar from '../assets/svgs/blackStar.svg?react'
+import GrayDiamond from '../assets/svgs/grayDiamond.svg?react'
+import BlackDiamond from '../assets/svgs/blackDiamond.svg?react'
 
 export function GigDetails() {
 
@@ -29,20 +34,43 @@ export function GigDetails() {
     rateInt=roundRate(gig.owner.rate)
 
     if (gig.owner.level==='basic'){
-      level='Level 1 ◆◇◇'
+      level='Level 1'
     } else if (gig.owner.level==='standard'){
-      level='Level 2 ◆◆◇'
+      level='Level 2'
     } else if (gig.owner.level==='premium'){
-      level='Level 3 ◆◆◆'
+      level='Level 3'
     }else{
       level=gig.owner.level
     }
+
+    gig.tags
   }
   if (!gig || gig._id!==gigId) return <p>Loading...</p> //when loading or swtichng gig
 
   return (
     <section className="gig-details">
-      <Link to="/gig">Back to list</Link>
+
+      <div className='nav-bar-line'>
+        
+        <ul className="nav-bar-list">
+          <li>
+            <NavLink className='home-link' to="/">
+              <Home/>
+            </NavLink>
+          </li>
+          <li className='slash'>
+            /
+          </li>
+          {gig.tags.map((tag, index) => (
+            <React.Fragment key={tag}>
+              <li><NavLink to="/gig">{tag}</NavLink></li>
+              {index < gig.tags.length - 1 && <li className='slash' key={`${tag}-separator`}>/</li>}
+            </React.Fragment>
+          ))}
+        </ul>
+      </div>
+     
+      {/* <Link to="/gig">Back to list</Link> */}
       {gig && <div className='gig-details-div'>
         <h1 className='gig-title'>{gig.title}</h1>
         <div className='owner-details-general'>
@@ -50,9 +78,25 @@ export function GigDetails() {
                 <div className='owner-details-data'>
                   <div className='name-level'>
                     <h5>{gig.owner.fullname}</h5>
-                    <h6>{level}</h6>
+                    {(level==='Level 1')&&<div className='owner-level'>
+                          <p>{level}</p> <BlackDiamond/> <GrayDiamond/> <GrayDiamond/>
+                    </div>}
+                    {(level==='Level 2')&&<div className='owner-level'>
+                          <p>{level}</p> <BlackDiamond/> <BlackDiamond/> <GrayDiamond/>
+                    </div>}
+                    {(level==='Level 3')&&<div className='owner-level'>
+                          <p>{level}</p> <BlackDiamond/> <BlackDiamond/> <BlackDiamond/>
+                    </div>}
+                    
                   </div>
-                  <p>{repeatIcon('★',rateInt)} {gig.owner.rate} </p>
+                  {/* <p>{repeatIcon('★',rateInt)} {gig.owner.rate} </p> */}
+                  <div className='rate'>
+                    {Array.from({ length: rateInt}, (_, index) => (
+                      <BlackStar key={index} />
+                    ))}
+                    <p>{gig.owner.rate}</p>
+                  </div>
+
                 </div>
 
         </div>
