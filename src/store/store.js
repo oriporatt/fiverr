7d0ce866cdb029ef0f38d65fdecfd19d4f0e7ed6
oriporatt <oriporatt@gmail.com>
2025-03-09 20:41:04 +0200

    added store of orders

--- src/store/store.js
+++ src/store/store.js
@@ -4,11 +4,14 @@ import { gigReducer } from './reducers/gig.reducer'
 import { userReducer } from './reducers/user.reducer'
 import { reviewReducer } from './reducers/review.reducer'
 import { systemReducer } from './reducers/system.reducer'
+import { orderReducer } from './reducers/order.reducer'
+
 
 const rootReducer = combineReducers({
     gigModule: gigReducer,
     userModule: userReducer,
     systemModule: systemReducer,
+    orderModule: orderReducer,
     reviewModule: reviewReducer,
 })
 
